6c250eb2019ab585534402563ad9b7a8b2c22530
Owen-292 <ob503@exeter.ac.uk>
2026-02-28 20:14:31 +0000

    added validation using a new method called inbounds, adjusted the add/remove obstacle because of this

--- src/main/java/cityrescue/CityMap.java
+++ src/main/java/cityrescue/CityMap.java
@@ -21,21 +21,25 @@ public class CityMap {
         return gridHeight;
     }
 
-    public void addObstacle(int x, int y){
-        try{
-            blocked[x][y] = true;
-        }
-        catch (InvalidLocationException e) {
-            throw new InvalidLocationException("Location is out of bounds ", e);
-        }
+    //method for validating a coordinate 
+    // x and y must be < and not =< width and height as these are always the number of valid coordinates +1
+    public boolean inBounds(int x, int y) {
+        return x >= 0 && x < gridWidth && y >= 0 && y < gridHeight;
     }
 
-    public void removeObstacle(int x, int y){
-        try{
-            blocked[x][y] = false;
-        }
-        catch (InvalidLocationException e){
-            throw new InvalidLocationException("Location is out of bounds ", e);
-        }
+    public void addObstacle(int x, int y) throws InvalidLocationException {
+        if (!inBounds(x, y)) throw new InvalidLocationException("Location out of bounds");
+        blocked[x][y] = true;
     }
+
+    public void removeObstacle(int x, int y) throws InvalidLocationException {
+        if (!inBounds(x, y)) throw new InvalidLocationException("Location out of bounds");
+        blocked[x][y] = false;
+    }
+
+    public boolean isBlocked(int x, int y) throws InvalidLocationException {
+        if (!inBounds(x, y)) throw new InvalidLocationException("Location out of bounds");
+        return blocked[x][y];
+    }
+    
 }
