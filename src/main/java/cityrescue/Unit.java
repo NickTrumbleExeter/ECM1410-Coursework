790a00c554e978f7ed4fd9627ced6be1708adf57
Owen-292 <ob503@exeter.ac.uk>
2026-02-28 16:37:07 +0000

    reworked the abstract unit class, added things like homestation and assignedincedentid. fixed the abstract methods by making them public. also one thing to note, the spec implies that ID assignment should be andled by the CityRescueImpl file so i removed that from this file. ill go through and update teh unit subclasses next to reflect these changes

--- src/main/java/cityrescue/Unit.java
+++ src/main/java/cityrescue/Unit.java
@@ -1,24 +1,31 @@
 package cityrescue;
 
-import cityrescue.enums.*;
+import cityrescue.enums.IncidentType;
+import cityrescue.enums.UnitStatus;
+import cityrescue.enums.UnitType;
 
 public abstract class Unit {    
+    
     private final int unitId;
+    private final UnitType unitType;
 
-    private UnitType unitType;
-    private UnitStatus status;
-    private int stationId;
+    private int homeStationId; // spec says to have each unit be assigned a home station id 
+
     private int x;
     private int y;
 
-    public static int unitIdCount = 0;
+    private UnitStatus status;
 
-    public Unit(UnitType type, int stationId){
-        this.unitId = unitIdCount;
-        this.stationId = stationId;
-        this.unitType = type;
+    private int assignedIncidentId; // -1 if none as -1 is ouside of ID range (IDs can only be >0)
+
+    protected Unit(int unitId, UnitType unitType, int homeStationId, int startX, int startY) {
+        this.unitId = unitId;
+        this.unitType = unitType;
+        this.homeStationId = homeStationId;
+        this.x = startX;
+        this.y = startY;
         this.status = UnitStatus.IDLE;
-        unitIdCount++;
+        this.assignedIncidentId = -1;
     }
 
     public int getUnitId(){
@@ -29,27 +36,55 @@ public abstract class Unit {
         return unitType;
     }
 
-    public int getStationId(){
-        return stationId;
+    public int getHomeStationId(){
+        return homeStationId;
+    }
+
+    public void setHomeStationId(int stationId) {
+        this.homeStationId = stationId;
     }
 
     public int[] getLocation(){
         return new int[]{x, y};
     }
 
+    // some methods for geting the unit's x and y which will be needed for calculating manhattan distance 
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setLocation(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     public UnitStatus getStatus(){
         return status;
     }
 
-    public void updateStationId(int newStationId){
-        stationId = newStationId;
-    }
-
-    public void updateStatus(UnitStatus status){
+    public void setStatus(UnitStatus status){
         this.status = status;
     }
 
-    abstract boolean canHandle(IncidentType incidentType);
+    //for resetting the unit so its now no longet assigned to an incedent 
+    public void clearAssignment() {
+        this.assignedIncidentId = -1;
+    }
 
-    abstract int getTicksToResolve(int severity);
+    // methods for getting and assigning incedent IDs
+        public int getAssignedIncidentId() {
+        return assignedIncidentId;
+    }
+
+    public void setAssignedIncidentId(int incidentId) {
+        this.assignedIncidentId = incidentId;
+    }
+
+    public abstract boolean canHandle(IncidentType type);
+
+    public abstract int getTicksToResolve(int severity);
 }
